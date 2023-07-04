a6966c3984ccae5b61a6d282adef4f1815a7d3c9
nalka <alan.audivert@hotmail.fr>
2023-07-04 10:37:21 +0200

    Initial commit

---
+++ Benchmark/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Benchmark;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using Perfolizer.Mathematics.Randomization;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var summary = BenchmarkRunner.Run<Benchmark>();
+    }
+}
+
+[SimpleJob]
+[RPlotExporter]
+public class Benchmark
+{
+    [Params(1000, 10000)]
+    public int N;
+
+    private List<double> list=new();
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        Shuffler shuffler = new();
+        list = new(Enumerable.Range(-N, N * 2).Select(x => (double)x));
+        shuffler.Shuffle(list);
+    }
+
+    [Benchmark]
+    public double OrderByFirst() => list.OrderBy(x => x).First();
+
+    [Benchmark]
+    public double MinBy() => list.MinBy(x => x);
+
+    [Benchmark]
+    public double OrderByDescendingLast() => list.OrderByDescending(x => x).Last();
+
+    [Benchmark]
+    public double OrderByDescendingFirst() => list.OrderByDescending(x => x).First();
+
+    [Benchmark]
+    public double MaxBy() => list.MaxBy(x => x);
+
+    [Benchmark]
+    public double OrderByLast() => list.OrderBy(x => x).Last();
+}
